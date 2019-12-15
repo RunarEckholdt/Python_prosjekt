413620f6ed96be3311c372f153a20f4377d89520
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2019-12-15 20:52:13 +0100

    fixed bug where non simplified expressions would not work

--- abc formelen.py
+++ abc formelen.py
@@ -7,6 +7,53 @@ Created on Mon Oct 14 13:00:41 2019
 import numpy as np
 
 
+def factorize(a):
+    isFactor = False
+    i = 2 
+    while not isFactor:
+        if(a%i==0):
+                isFactor=True
+        else:
+            i+=1
+    return i    
+    
+
+def simplify(a,b,c):
+    aList = []
+    bList = []
+    cList = []
+    tempA = abs(a)
+    tempB = abs(b)
+    tempC = abs(c)
+    
+    
+    while tempA>1:
+        aList.append(factorize(tempA))  #finner neste faktor for tallet og legger den til i listen
+        tempA= tempA/aList[-1]          #"fjerner" faktoren fra tallet ved å dividere den ut -1 for siste verdi i listen
+    while tempB>1:
+        bList.append(factorize(tempB))
+        tempB = tempB/bList[-1]
+    while tempC>1:
+        cList.append(factorize(tempC))
+        tempC = tempC/cList[-1]
+    
+    for x in range(len(aList)):
+        if((aList[x]in bList or b==0) and (aList[x]in cList or c == 0)): #hvis verdien i aList for index x er faktor i både bList og cList
+            print("Factorized out",aList[x])
+            a = a/aList[x]
+            b = b/aList[x]
+            c = c/aList[x]
+            try:
+                bList.pop(bList.index(aList[x])) #fjerner faktorene ettersom de er "dividert" bort
+            except:
+                print("No index for the factor in b")
+            try:
+                cList.pop(cList.index(aList[x])) 
+            except:
+                print("No index for the factor in c")
+     
+    return [a,b,c]
+
 def abc_formel1(a, b, c):
     #(-b+sqrt(b^2-4*a*c))2*a
     try:
@@ -36,6 +83,7 @@ def abc_formel2(a, b, c):
         print("Error...")
         main()
 
+
 def main():
     a = input("Type in a: ")
     b = input("Type in b: ")
@@ -48,8 +96,15 @@ def main():
     except:
         print("That is not a valid input")
         main()
-        
-
+    print("Solving expression " + str(a) +"x^2 " + str(b) + "x "+ str(c) + " = 0")
+    simplified = simplify(a,b,c)
+    a = int(simplified[0])
+    b = int(simplified[1])
+    c = int(simplified[2])
+    print(c)
+    
+    print("Expression Simplified to: "+ str(a) + "x^2 " + str(b) + "x " + str(c) + " = 0")
+    
     x1 = abc_formel1(a,b,c)
     x2 = abc_formel2(a,b,c)
     
