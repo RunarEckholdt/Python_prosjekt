da01dc77ef49ddd8883512762762287ed5cef813
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2020-01-07 15:15:26 +0100

    akselerasjons oppgaven

---
+++ Plotte akselerajons graf.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 14:51:17 2020
+
+@author: Runar
+"""
+
+#import sympy as sy
+import numpy as np
+import matplotlib.pyplot as plt
+
+#t = sy.symbol('t')
+A = 2.5
+t = 5
+
+x = np.linspace(0,9,40)
+
+def akselerasjon(A, t):
+    a = A * np.exp(-x/t)
+    return a
+
+plt.xlabel("t")
+plt.ylabel("a")
+plt.plot(x,akselerasjon(A,t),"b--")
+plt.show()
