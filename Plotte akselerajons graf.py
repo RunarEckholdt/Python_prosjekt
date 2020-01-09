526815bd9f2ba09fff6ef6ab3162f47a78e60cc3
Runar Eckholdt <56927937+RunarEckholdt@users.noreply.github.com>
2020-01-09 21:33:38 +0100

    added fart function

--- Plotte akselerajons graf.py
+++ Plotte akselerajons graf.py
@@ -19,7 +19,16 @@ def akselerasjon(A, t):
     a = A * np.exp(-x/t)
     return a
 
+def fart(A,t):
+    v = (-1/5)*A*np.exp(-x/t) + 12
+    return v
+
 plt.xlabel("t")
 plt.ylabel("a")
-plt.plot(x,akselerasjon(A,t),"b--")
+plt.plot(x,akselerasjon(A,t),"b-")
 plt.show()
+
+plt.ylabel("v")
+plt.plot(x,fart(A,t), "r-")
+
+print((-1/5)*A*np.exp(-9/t)+12.5)
